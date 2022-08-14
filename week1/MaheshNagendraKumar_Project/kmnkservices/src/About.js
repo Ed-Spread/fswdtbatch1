2bb3e57d0a8aa3c28fc30b621fd3329292b3c9e5
Mahesh Nagendra Kumar <68604040+Maheshkmnk@users.noreply.github.com>
2022-08-14 12:43:02 +0530

    Add files via upload

--- week1/MaheshNagendraKumar_Project/kmnkservices/src/About.js
+++ week1/MaheshNagendraKumar_Project/kmnkservices/src/About.js
@@ -7,6 +7,8 @@ function About() {
         <h1 className='captions'>My Wedding Planning - Your Destination Wedding Specialist</h1>
         <h2 className='captions'>Indian Wedding Specialist</h2>
         <span><img className='image' src='https://www.myweddingplanning.in/wp-content/uploads/umaid-bhavan-wedding-1.jpg'/></span>
+        <span><img className='image'src='https://www.myweddingplanning.in/wp-content/uploads/royal-wedding-decor-5.jpg'/></span>
+        <span><img className='image'src='https://www.myweddingplanning.in/wp-content/uploads/Sangeet-Stage-Design-by-Elite-Wedding-Planner-1.jpg'/></span>
         
         
         <span className='span1'>
@@ -16,18 +18,18 @@ function About() {
              accompanying you not only on your initial viewing of possible venues but on any subsequent visits and for 
              the duration of your wedding, ensuring total dedication to you.
         </p></span>
-        <span><img className='image'src='https://www.myweddingplanning.in/wp-content/uploads/Sangeet-Stage-Design-by-Elite-Wedding-Planner-1.jpg'/></span>
         <p className='paraGraph'>At, My Wedding Planning, Destination Wedding Planning is our strongest competence.
          So if you are planning a destination wedding, let us enhance your wedding experience with our
           expertise and flawless execution.
         </p>
-        <span><img className='image'src='https://www.myweddingplanning.in/wp-content/uploads/royal-wedding-decor-5.jpg'/></span>
+        
         <p className='paraGraph'>We offer a wide range of services from full service design and execution to
             wedding day coordination; and can be as involved as you want us to be. We ensure 
             flawless design, personalized service and exceptional attention to detail. Every detail
             of your wedding will be meticulously managed by the exceptional planning capabilities,
             contacts and resources of the My Wedding Planning production team. We will ensure your special day has a wow-factor
         </p>
+        
     </div>
   )
 }
--- week1/MaheshNagendraKumar_Project/kmnkservices/src/Header.js
+++ week1/MaheshNagendraKumar_Project/kmnkservices/src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css'
 import {signInWithGoogle, auth, provider} from './firebase'
+import { Link } from "react-router-dom";
 
 var name = localStorage.getItem("name")
 var profilePic = localStorage.getItem("profilePic")
@@ -12,24 +13,33 @@ function Header() {
     
   return (
     <div className='header'>
-        <img className='header_logo' src="https://i.ibb.co/1ZsrXyQ/94417434-padded-logo.png" alt="94417434-padded-logo" border="0" />
+        <Link to='/'>
+            <img className='header_logo' src="https://i.ibb.co/1ZsrXyQ/94417434-padded-logo.png" alt="94417434-padded-logo" border="0" />
+        </Link>
+       
         <div className='header_nav'>
 
             <div className='header_nav_items'>
-                <button className='button' onClick={name?handleAuthenticaton:signInWithGoogle}>{name?'Sign out':'Sign in'}</button>
+                <button className='button' onClick={name?handleAuthenticaton:signInWithGoogle}>{name?'Welcome'+' '+name+' '+'Signout':'Sign in'}</button>
             </div>
-        
-            <div className='header_nav_items'>
-                <span>About</span>
-            </div>
-
-            <div className='header_nav_items'>
-                <span>Events</span>
-            </div>
-
-            <div className='header_nav_items'>
-                <span>Contact</span>
-            </div>
+            <Link to='/About'>
+                <div className='header_nav_items'>
+                    <span>About</span>
+                </div>
+            </Link>
+            
+            <Link to='/Events'>
+                <div className='header_nav_items'>
+                    <span>Events</span>
+                </div>
+            </Link>
+            
+            <Link to='/Contact'>
+                <div className='header_nav_items'>
+                    <span>Contact</span>
+                </div>
+            </Link>
+            
         </div>
     </div>
   )
